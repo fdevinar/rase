20e5515b913bb90f001737589b6c8ad2e822df5e
fdevinar <fc_devinar@hotmail.com>
2026-03-26 19:16:03 -0300

    created schedule execution response mapper

---
+++ src/main/java/com/fabricio/rase/application/ScheduleExecutionResponseMapper.java
@@ -0,0 +1,38 @@
+package com.fabricio.rase.application;
+
+import com.fabricio.rase.application.dto.ScheduleExecutionResponse;
+import com.fabricio.rase.application.dto.ShiftExecutionResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScheduleExecutionResponseMapper {
+
+    public ScheduleExecutionResponse map (ExecuteScheduleResult result) {
+
+        ExecutionReport executionReport = result.executionReport();
+        PolicyResults policyResults = result.policyResults();
+
+        List<ShiftExecutionResponse> shiftResults = new ArrayList<>();
+        for (ShiftResult shiftResult : executionReport.results()) {
+            ShiftExecutionResponse shiftResponse =
+                    new ShiftExecutionResponse(
+                            shiftResult.shiftId(),
+                            shiftResult.successful(),
+                            shiftResult.failureType() != null ? shiftResult.failureType().toString() : null,
+                            shiftResult.failureMessage()
+                    );
+            shiftResults.add(shiftResponse);
+        }
+        return new ScheduleExecutionResponse(
+                executionReport.totalShifts(),
+                executionReport.successfulShifts(),
+                executionReport.failedShifts(),
+                shiftResults,
+                policyResults.userExecution().toString(),
+                policyResults.suggestedAction().toString()
+                );
+    }
+
+}
+
