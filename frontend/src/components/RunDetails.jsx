90e93cbe546c0f517809ef535690a4343235f123
fdevinar <fc_devinar@hotmail.com>
2026-05-21 17:50:06 -0300

    added shift execution overview

--- frontend/src/components/RunDetails.jsx
+++ frontend/src/components/RunDetails.jsx
@@ -2,13 +2,35 @@ import { formatDate } from "../utils/utils";
 
 export default function RunDetails({ run }) {
     
-    // console.log(run);        
-    const report = run?.resultJson; 
-    console.log(report);
-
+    // console.log(run);
+    const report = run?.resultJson;    
+    let executionReport;
+    let policyResults;
+    let workerResults;
+    let successRate;
+    if (report) {
+        const parsedReport = JSON.parse(report);        
+        executionReport = parsedReport.executionReport;
+        policyResults = parsedReport.policyResults;
+        workerResults = parsedReport.workerResults;
+        successRate = (executionReport.successfulShifts / executionReport.totalShifts) * 100 ;
+        console.log("** Execution Report **");
+        console.log(executionReport);
+        console.log("** Policy Results **");
+        console.log(policyResults);
+        console.log("** Worker Results **");
+        console.log(workerResults);
+        console.log("** Success Rate **");
+        console.log(successRate);
+    }
+        
     return (
 
-        <div className="details-wrapper">            
+        <div className="details-wrapper">   
+
+            <h2>Run Details</h2>
+
+            {/* BASICS */}
             <div className="header-row">
                 <div className="header-item">ID</div>
                 <div className="header-item">Status</div>
@@ -23,11 +45,39 @@ export default function RunDetails({ run }) {
                 <p>{formatDate(run.createdAt)}</p>
                 </div>
 
-                <div className="results">
+                {/* <div className="results">
                 <p>{run.resultJson}</p>
+                </div> */}
+            </>
+            )}     
+
+            {/* EXECUTION REPORT */}
+
+            {executionReport && (
+            <>
+                <h2>Shift Execution Overview</h2>
+                <div className="execution-report">
+                    <div className="execution-item total">
+                        <p className="number">{executionReport.totalShifts}</p>
+                        <p className="text">Total Shifts</p>
+                    </div>
+                    <div className="execution-item success">
+                        <p className="number">{executionReport.successfulShifts}</p>
+                        <p className="text">Successful</p>
+                    </div>
+                    <div className="execution-item fail">
+                        <p className="number">{executionReport.failedShifts}</p>
+                        <p className="text">Failed</p>
+                    </div>
+                    <div className="execution-item percentage">
+                        <p className="number">{successRate.toFixed(0)} %</p>
+                        <p className="text">Success Rate</p>
+                    </div>
                 </div>
             </>
-            )}        
+            )
+
+            }
                                                               
         </div>
 
